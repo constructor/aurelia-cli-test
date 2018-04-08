c1388feb730f37089bdb32f2f12ceeda020f4e97
constructor <john@designs.digital>
2018-04-08 18:57:05 +0100

    Just some basic UI test content

--- src/pages/page-detail.ts
+++ src/pages/page-detail.ts
@@ -2,12 +2,13 @@ import { bindable, inject } from 'aurelia-framework';
 
 export class PageDetail {
   message = 'Hello World! Page Detail';
-
+  params: any;
   constructor() {
   }
 
   activate(params, routeConfig) {
     console.log(params);
+    this.params = params;
   }
 
 }
